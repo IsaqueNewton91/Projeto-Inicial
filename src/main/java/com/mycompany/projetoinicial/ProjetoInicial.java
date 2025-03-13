7f3bfc63edc8f3e8256fba8205d8ba32cd1d5933
Isaque Newton <isaquerapiso@gmail.com>
2025-03-12 23:15:27 -0300

    Projeto Inicial

--- src/main/java/com/mycompany/projetoinicial/ProjetoInicial.java
+++ src/main/java/com/mycompany/projetoinicial/ProjetoInicial.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.projetoinicial;
 
-/**
- *
- * @author isaqu
- */
+
 public class ProjetoInicial {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Projeto Inicial!");
     }
 }
